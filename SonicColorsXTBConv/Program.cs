526e1345d37400397eddd925d20af4575b52dd7a
Sergei Oganesov <91889396+TheExentist151@users.noreply.github.com>
2023-08-25 10:59:16 +0600

    Bump version to 1.2

--- SonicColorsXTBConv/Program.cs
+++ SonicColorsXTBConv/Program.cs
@@ -6,7 +6,7 @@
         {
             if(args.Length == 0)
             {
-                Console.WriteLine("SonicColorsXTBConv v1.1\nUsage: SonicColorsXTBConv <source file>");
+                Console.WriteLine("SonicColorsXTBConv v1.2\nUsage: SonicColorsXTBConv <source file>");
                 return;
             }
             else
